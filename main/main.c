ef9a56c8077130df86373c9a45d202017f047dfe
Tan <tanldphan@outlook.com>
2025-07-16 21:45:52 -0700

    Stop tracking build folder

--- main/main.c
+++ main/main.c
@@ -12,32 +12,33 @@
 #include "esp_sleep.h"
 #include "esp_system.h"
 
+// UncleRus
 #include "ds3231.h"
 #include "i2cdev.h"
 
-#define RTC_I2C I2C_NUM_0 // assigning ESP's I2C controller 0
+#define RTC_I2C I2C_NUM_0
 #define RTC_SDA (10)
 #define RTC_SCL (11)
 #define RTC_SQW (21)
 
-#define WAKE "WAKE UP"
-
-// Not being run at boot >> means wake up from deep sleep
+// This only runs this on cold boot
 struct tm fake_time = {
     .tm_year = 2025 - 1900,
-    .tm_mon  = 6,   // July
+    .tm_mon  = 6,
     .tm_mday = 14,
     .tm_hour = 15,
     .tm_min  = 0,
     .tm_sec  = 0
 };
 
-void app_main(void) // >>>>> Starts here after wake from deep sleep
+void app_main(void) // >>>>> Runs on cold or sleep
 {
     i2cdev_init();
 
     esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
-    ESP_LOGI(WAKE, "Wakeup cause: %d (%s)", cause,
+
+    // Log boot cause
+    ESP_LOGI("WAKE", "cause: %d (%s)", cause,
     (cause == ESP_SLEEP_WAKEUP_UNDEFINED) ? "Cold boot" :
     (cause == ESP_SLEEP_WAKEUP_EXT0) ? "EXT0 RTC Alarm" :
     "Other");
@@ -45,27 +46,24 @@ void app_main(void) // >>>>> Starts here after wake from deep sleep
     i2c_dev_t dev = { 0 };
 
     ESP_ERROR_CHECK(ds3231_init_desc(&dev, RTC_I2C, RTC_SDA, RTC_SCL));
-    printf("DS3231 descriptor initialized\n"); fflush(stdout);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    
-    if (ds3231_clear_alarm_flags(&dev, DS3231_ALARM_1) == ESP_OK)
-    {
-        printf("Alarm cleared!\n"); fflush(stdout);
-    }
+    vTaskDelay(pdMS_TO_TICKS(100));
 
+    // Reset clock if cold boot
     if (cause == ESP_SLEEP_WAKEUP_UNDEFINED)
     {
         ds3231_set_time(&dev, &fake_time);
-        printf("Cold boot: Fake time set!\n"); fflush(stdout);
     }
 
+    ds3231_clear_alarm_flags(&dev, DS3231_ALARM_1);
+
     struct tm alarm_time;
-    if (ds3231_get_time(&dev, &alarm_time) == ESP_OK)
+    if (ds3231_get_time(&dev, &alarm_time) != ESP_OK)
     {
-        printf("Pasted time!\n"); fflush(stdout);
+    ESP_LOGE("RTC", "Failed to read current time");
+    return;
     }
-    
-    alarm_time.tm_sec += 60;
+
+    alarm_time.tm_sec += 30;
     mktime(&alarm_time);
 
     ds3231_set_alarm(&dev, DS3231_ALARM_1, &alarm_time, DS3231_ALARM1_MATCH_SECMIN, NULL, 0);
@@ -73,6 +71,7 @@ void app_main(void) // >>>>> Starts here after wake from deep sleep
 
     ds3231_enable_alarm_ints(&dev, DS3231_ALARM_1);
 
+    // SQW's pull-up enabled
     gpio_config_t io_conf = {
         .pin_bit_mask = 1ULL << RTC_SQW,
         .mode = GPIO_MODE_INPUT,
@@ -83,9 +82,13 @@ void app_main(void) // >>>>> Starts here after wake from deep sleep
 
     gpio_config(&io_conf);
 
+    // Let SQW wake ESP on LOW
     esp_sleep_enable_ext0_wakeup(RTC_SQW, 0);
 
-    i2cdev_done();  // Frees any semaphores still hanging around
+    // Frees any semaphores still hanging around
+    i2cdev_done();
+
+    // Sleep countdown
     ESP_LOGI("ABOUT TO SLEEP", "5s to deep sleep");
     vTaskDelay(pdMS_TO_TICKS(1000 * 5));
 
